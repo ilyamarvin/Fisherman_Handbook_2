af1ec20598e1c2be1275dbf5b750ebdcc3a085e6
ilyamarvin <ilya.sereda2002@mail.ru>
2020-05-17 19:08:11 +0300

    Added an expandable layout to HooksCategory and RigCategory.

--- app/src/main/java/com/ilyamarvin/fishermanhandbook2/MenuCategories/HooksCategory.java
+++ app/src/main/java/com/ilyamarvin/fishermanhandbook2/MenuCategories/HooksCategory.java
@@ -8,8 +8,11 @@ import android.widget.ImageView;
 
 import com.ilyamarvin.fishermanhandbook2.R;
 
+import net.cachapa.expandablelayout.ExpandableLayout;
+
 public class HooksCategory extends AppCompatActivity {
     ImageView backBtn;
+    ExpandableLayout expandableLayout1, expandableLayout2, expandableLayout3, expandableLayout4, expandableLayout5, expandableLayout6, expandableLayout7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +27,40 @@ public class HooksCategory extends AppCompatActivity {
                 HooksCategory.super.onBackPressed();
             }
         });
+    }
+
+    public void expandableButtonHooks1(View view) {
+        expandableLayout1 = findViewById(R.id.expandableLayoutHooks1);
+        expandableLayout1.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonHooks2(View view) {
+        expandableLayout2 = findViewById(R.id.expandableLayoutHooks2);
+        expandableLayout2.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonHooks3(View view) {
+        expandableLayout3 = findViewById(R.id.expandableLayoutHooks3);
+        expandableLayout3.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonHooks4(View view) {
+        expandableLayout4 = findViewById(R.id.expandableLayoutHooks4);
+        expandableLayout4.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonHooks5(View view) {
+        expandableLayout5 = findViewById(R.id.expandableLayoutHooks5);
+        expandableLayout5.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonHooks6(View view) {
+        expandableLayout6 = findViewById(R.id.expandableLayoutHooks6);
+        expandableLayout6.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonHooks7(View view) {
+        expandableLayout7 = findViewById(R.id.expandableLayoutHooks7);
+        expandableLayout7.toggle(); // toggle expand and collapse
     }
 }
--- app/src/main/java/com/ilyamarvin/fishermanhandbook2/MenuCategories/RigCategory.java
+++ app/src/main/java/com/ilyamarvin/fishermanhandbook2/MenuCategories/RigCategory.java
@@ -8,8 +8,11 @@ import android.widget.ImageView;
 
 import com.ilyamarvin.fishermanhandbook2.R;
 
+import net.cachapa.expandablelayout.ExpandableLayout;
+
 public class RigCategory extends AppCompatActivity {
     ImageView backBtn;
+    ExpandableLayout expandableLayout1, expandableLayout2, expandableLayout3, expandableLayout4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +27,25 @@ public class RigCategory extends AppCompatActivity {
                 RigCategory.super.onBackPressed();
             }
         });
+    }
+
+    public void expandableButtonRig1(View view) {
+        expandableLayout1 = findViewById(R.id.expandableLayoutRig1);
+        expandableLayout1.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonRig2(View view) {
+        expandableLayout2 = findViewById(R.id.expandableLayoutRig2);
+        expandableLayout2.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonRig3(View view) {
+        expandableLayout3 = findViewById(R.id.expandableLayoutRig3);
+        expandableLayout3.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonRig4(View view) {
+        expandableLayout4 = findViewById(R.id.expandableLayoutRig4);
+        expandableLayout4.toggle(); // toggle expand and collapse
     }
 }
