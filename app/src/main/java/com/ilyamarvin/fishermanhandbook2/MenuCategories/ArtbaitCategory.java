49517ff5df2cca099c63206607e566e2055da750
ilyamarvin <ilya.sereda2002@mail.ru>
2020-05-17 19:26:59 +0300

    Added an expandable layout to ArtbaitCategory.

--- app/src/main/java/com/ilyamarvin/fishermanhandbook2/MenuCategories/ArtbaitCategory.java
+++ app/src/main/java/com/ilyamarvin/fishermanhandbook2/MenuCategories/ArtbaitCategory.java
@@ -8,8 +8,11 @@ import android.widget.ImageView;
 
 import com.ilyamarvin.fishermanhandbook2.R;
 
+import net.cachapa.expandablelayout.ExpandableLayout;
+
 public class ArtbaitCategory extends AppCompatActivity {
     ImageView backBtn;
+    ExpandableLayout expandableLayout1, expandableLayout2, expandableLayout3, expandableLayout4, expandableLayout5, expandableLayout6, expandableLayout7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +27,40 @@ public class ArtbaitCategory extends AppCompatActivity {
                 ArtbaitCategory.super.onBackPressed();
             }
         });
+    }
+
+    public void expandableButtonArtbait1(View view) {
+        expandableLayout1 = findViewById(R.id.expandableLayoutArtbait1);
+        expandableLayout1.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonArtbait2(View view) {
+        expandableLayout2 = findViewById(R.id.expandableLayoutArtbait2);
+        expandableLayout2.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonArtbait3(View view) {
+        expandableLayout3 = findViewById(R.id.expandableLayoutArtbait3);
+        expandableLayout3.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonArtbait4(View view) {
+        expandableLayout4 = findViewById(R.id.expandableLayoutArtbait4);
+        expandableLayout4.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonArtbait5(View view) {
+        expandableLayout5 = findViewById(R.id.expandableLayoutArtbait5);
+        expandableLayout5.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonArtbait6(View view) {
+        expandableLayout6 = findViewById(R.id.expandableLayoutArtbait6);
+        expandableLayout6.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButtonArtbait7(View view) {
+        expandableLayout7 = findViewById(R.id.expandableLayoutArtbait7);
+        expandableLayout7.toggle(); // toggle expand and collapse
     }
 }
